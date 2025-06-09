4b868600a2103fa7d834a83854f0af8796bad410
Arijit78 <sahaarijit2007@gmail.com>
2025-06-09 07:11:30 +0000

    alioth: LineageOS BringUp

--- lineage_alioth.mk
+++ lineage_alioth.mk
@@ -7,27 +7,23 @@
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-# Inherit some common AospExtended stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_alioth
+PRODUCT_NAME := lineage_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_BRAND := POCO
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := POCO F3
-TARGET_SCREEN_DENSITY:=450
-
-TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# AEX
+# Inherit some common device props
+TARGET_SCREEN_DENSITY := 450
+TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
 USE_PIXEL_CHARGER_IMAGES := true
-TARGET_SHIP_GCAM_GO := true
-TARGET_SUPPORTS_BLUR := true
 PRODUCT_BOARD_PLATFORM := kona
 PRODUCT_USES_QCOM_HARDWARE := true
-WITH_GMS := true
-WITH_GAPPS := true
+WITH_GMS := false
